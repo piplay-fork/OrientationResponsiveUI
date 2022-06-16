b1636437c44da99145c90a40aa3ae0744bddc26b
paynechu <314176+paynechu@users.noreply.github.com>
2022-06-16 11:55:22 +0800

    Improved to fully Stroryboard driven
    - without programmatically adding constraints
    - fixed a bug, that triggered if upside down not enabled. after go though the upside down the constraints will not work properly
    - moved the orientation code under viewDidLayoutSubviews, Storyboard driven way will only work in this method to deactivate and activate constraints

--- OrientationResponsiveUI/ViewController.swift
+++ OrientationResponsiveUI/ViewController.swift
@@ -10,26 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var IBConstraints: [NSLayoutConstraint]!
-    var landscapeConstraints: [NSLayoutConstraint] = []
+    @IBOutlet var portraitConstraints: [NSLayoutConstraint]!
+    @IBOutlet var landscapeConstraints: [NSLayoutConstraint]!
     
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
     @IBOutlet weak var view3: UIView!
     
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        NotificationCenter.default.addObserver(self, selector: #selector(self.orientationChanged), name: UIApplication.didChangeStatusBarOrientationNotification, object: nil)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         view1.backgroundColor = greenColors[0]
         view2.backgroundColor = greenColors[1]
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            fallthrough
+        case .portraitUpsideDown:
+            print("Portrait")
+            NSLayoutConstraint.deactivate(landscapeConstraints)
+            NSLayoutConstraint.activate(portraitConstraints)
+        case .landscapeLeft:
+            fallthrough
+        case .landscapeRight:
+            print("Landscape")
+            NSLayoutConstraint.deactivate(portraitConstraints)
+            NSLayoutConstraint.activate(landscapeConstraints)
+        case .unknown:
+            print("unknown orientation")
+        @unknown default:
+            print("unknown case in orientation change")
+        }
     }
     
     @IBAction func button1Action(_ sender: Any) {
@@ -45,38 +58,5 @@ class ViewController: UIViewController {
     @IBAction func button3Action(_ sender: Any) {
         view1.backgroundColor = blueColors[0]
         view2.backgroundColor = blueColors[1]
-    }
-    
-    @objc func orientationChanged(notification: NSNotification) {
-        let deviceOrientation = UIApplication.shared.statusBarOrientation
-
-        switch deviceOrientation {
-        case .portrait:
-            fallthrough
-        case .portraitUpsideDown:
-            print("Portrait")
-            self.applyPortraitConstraints()
-
-        case .landscapeLeft:
-            fallthrough
-        case .landscapeRight:
-            print("landscape")
-            self.applyLandscapeConstraints()
-        case .unknown:
-            print("unknown orientation")
-        @unknown default:
-            print("unknown case in orientation change")
-        }
-    }
-    
-    func applyLandscapeConstraints() {
-        NSLayoutConstraint.deactivate(IBConstraints)
-        
-        landscapeConstraints = ConstraintsHelper.applyLandscapeConstraints(view: self.view, view1: view1, view2: view2, view3: view3)
-    }
-    
-    func applyPortraitConstraints() {
-        NSLayoutConstraint.deactivate(landscapeConstraints)
-        view.addConstraints(IBConstraints)
     }
 }
